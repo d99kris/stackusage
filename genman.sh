30881e2e08205bb312a4288cfe01619225e1d749
Kristofer Berggren <kristofer.berggren@gmail.com>
2019-10-31 19:54:14 +0800

    update genman script to also perform build

--- genman.sh
+++ genman.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 
-cd src && help2man -n "measure stack usage in applications" -N -o stackusage.1 ./stackusage
+mkdir -p build && cd build && cmake .. && make -s && \
+help2man -n "measure stack usage in applications" -N -o stackusage.1 ./stackusage
